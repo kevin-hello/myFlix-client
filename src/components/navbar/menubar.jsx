ed1d4e8cc22dfe67427a462a97fe57b55e936125
Kevin Schaefer <kevinss.hello@gmail.com>
2022-01-21 19:52:28 -0800

    nav bar created

---
+++ src/components/navbar/menubar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Navbar, Container, Nav, Button} from 'react-bootstrap';
+
+export default function Menubar ({user}) {
+
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  }
+
+
+
+  const isAuth = () => {
+    if(typeof window == "undefined") {
+      return false;
+    }
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
+
+  return(
+    <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
+      <Container>
+      <Navbar.Brand className="navbar-logo" href="/">myFlix</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              {isAuth() && (
+                <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
+              )}
+              {isAuth() && (
+                <Button variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
+              )}
+              {!isAuth() && (
+                <Nav.Link href="/">Login</Nav.Link>
+              )}
+               {!isAuth() && (
+                <Nav.Link href="/register">Register</Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse> 
+      </Container>
+    </Navbar>
+  );
+}
+
+

4009641cbf5c104565df193c5b222403fdceade6
Kevin Schaefer <kevinss.hello@gmail.com>
2022-01-31 14:38:39 -0800

    removed unnecessary files

--- src/components/registration-view/register-button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import './register-button.scss'
-
-export const RegisterButton = (props) => {
-  return <Button id= "register" onClick={props.addTrip}>Create Account</Button>
-}
-
-export default RegisterButton

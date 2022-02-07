e8cd0ce52e495c247480bfe692ac19f580fa4200
Kevin Schaefer <kevinss.hello@gmail.com>
2022-02-06 23:17:59 -0800

    basic movie card added, back button fixed,  class component changed to function

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 //UI elements
-import { Col, Button} from 'react-bootstrap';
+import { Row, Col, Button, Container} from 'react-bootstrap';
+
+import { BasicMovieCard } from '../basic-movie-card/basic-movie-card';
 //styling
 import "./genre-view.scss";
 
-export class GenreView extends React.Component {
-  render(){
-  const {genre, onBackClick } = this.props;
+export function GenreView ({movie, movies, onBackClick}) {
   
   return(
+    <Container>
+   
+    <Row>
     <Col className="genre-view">
       <div className="genre-name">
-        <span className="label">Name: </span>
-        <span className="value">{genre.Name}</span>
+        <h1>{movie.Genre.Name}</h1>
       </div>
       <div className="genre-description">
         <span className="label">Description: </span>
-        <span className="value">{genre.Description}</span>
+        <span className="value">{movie.Genre.Description}</span>
       </div>
-      <Link to={`/`}>
-      <Button id="return" variant="secondary" onClick={() => onBackClick(null)}>Back</Button>
-      </Link>
-
+      <Button id="return" variant="secondary" onClick={() => onBackClick()}>Back</Button>
     </Col>
+    </Row>
+    <Row className='genre-title'>
+      <h3>{movie.Genre.Name} Movies</h3>
+    </Row>
+    <Row>
+    { movies && movies.map((movie) => (
+    <Col md={3} key={movie._id}>
+    <div className="genreMovieDiv" >
+    <BasicMovieCard movie={movie} />
+    </div>
+    </Col> ))}
+    </Row>
+
+
+</Container>
+
   );
-}}
+}
